20d9053ee1a9e73e92975a7bd509c7185f1a13d2
Justinel2 <justine.lardeux@hotmail.com>
2018-11-05 22:02:42 -0500

    P2: Preload paddles in an array

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -20,20 +20,28 @@ var board;
 var timer;
 var interval = 5000;
 
-// Variable containing the macromolecule icons
+// Array containing the macromolecule icons
 var macros = [];
 
+// Array containing the paddles
+var paddles = [];
 
 // preload()
 //
 // Loads the different media
 function preload() {
+  // Preload images for macromolecules
   macros[0] = loadImage('assets/images/polysac_icon.png');
   macros[1]= loadImage('assets/images/disac_icon.png');
-  macros[2] = loadImage('assets/images/monosac_icon.png');
-  macros[3] = loadImage('assets/images/lipid_icon.png');
-  macros[4] = loadImage('assets/images/protein_icon.png');
-  macros[5] = loadImage('assets/images/nuclacid_icon.png');
+  macros[2] = loadImage('assets/images/lipid_icon.png');
+  macros[3] = loadImage('assets/images/protein_icon.png');
+  macros[4] = loadImage('assets/images/nuclacid_icon.png');
+
+  // Preload images for paddles
+  paddle[0] = loadImage('assets/images/topPaddle.png');
+  paddle[1] = loadImage('assets/images/rightPaddle.png');
+  paddle[2] = loadImage('assets/images/bottomPaddle.png');
+  paddle[3] = loadImage('assets/images/rightPaddle.png');
 }
 
 // setup()
@@ -49,12 +57,12 @@ function setup() {
     balls.push(new Ball((width-445.542)/2,height/2,random(-5,5),random(-5,5),50,5,macros[r]));
   }
   // Create the right paddle with UP and DOWN as controls
-  rightPaddle = new Paddle((width-445.542),height/2,10,60,10,40,38,37,39);
+  rightPaddle = new Paddle((width-445.542),height/2,10,100,10,40,38,37,39);
   // Create the left paddle with W and S as controls
   // Keycodes 83 and 87 are W and S respectively
-  leftPaddle = new Paddle(40,height/2,10,60,10,40,38,37,39);
-  topPaddle = new Paddle((width-395.542)/2,40,60,10,10,40,38,37,39);
-  bottomPaddle = new Paddle((width-395.542)/2,height-50,60,10,10,40,38,37,39);
+  leftPaddle = new Paddle(40,height/2,10,100,10,40,38,37,39);
+  topPaddle = new Paddle((width-395.542)/2,40,100,10,10,40,38,37,39);
+  bottomPaddle = new Paddle((width-395.542)/2,height-50,100,10,10,40,38,37,39);
   board = new Board((width-395.542),0,395.542,height);
 }
 
