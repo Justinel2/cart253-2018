35b5871584a3eb3b8e69c7253a37e0254f5a4451
Justinel2 <justine.lardeux@hotmail.com>
2018-12-04 22:58:13 -0500

    P3: Create the Textbox.js class

---
+++ projects/project3/js/Textbox.js
@@ -0,0 +1,15 @@
+// Textbox.js
+//
+// A class to define how to display the text box (the area dedicated for t
+// generated text)
+//
+
+// // Textbox constructor
+// //
+// // Sets the properties with the provided arguments
+// function Textbox(x,y,w,h) {
+//   this.x = x;
+//   this.y = y;
+//   this.w = w;
+//   this.h = h;
+// }
